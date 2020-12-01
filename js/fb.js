2e196ac54ff420a8bfaf908c1ebff01b13105a5c
fangdai <pq.dai225@gmail.com>
2020-12-01 14:45:52 +0700

    Update fb.js

---
+++ js/fb.js
@@ -0,0 +1,15 @@
+ // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyBVri71wDojtK31uS46pZTQEwvutheY_b8",
+    authDomain: "hydroponic-garden-khmt.firebaseapp.com",
+    databaseURL: "https://hydroponic-garden-khmt.firebaseio.com",
+    projectId: "hydroponic-garden-khmt",
+    storageBucket: "hydroponic-garden-khmt.appspot.com",
+    messagingSenderId: "968097949384",
+    appId: "1:968097949384:web:9e6e8feddaf8e2a530a7fd",
+    measurementId: "G-RBHNLGGD1S"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
