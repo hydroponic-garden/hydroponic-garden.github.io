cacf923d8734fb6192541cb66eb8ce94e1e614f5
fangdai <pq.dai225@gmail.com>
2020-12-18 00:13:51 +0700

    registerForm

--- js/fb.js
+++ js/fb.js
@@ -1,6 +1,4 @@
- // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  var firebaseConfig = {
+var firebaseConfig = {
     apiKey: "AIzaSyBVri71wDojtK31uS46pZTQEwvutheY_b8",
     authDomain: "hydroponic-garden-khmt.firebaseapp.com",
     databaseURL: "https://hydroponic-garden-khmt.firebaseio.com",
@@ -10,7 +8,7 @@
     appId: "1:968097949384:web:9e6e8feddaf8e2a530a7fd",
     measurementId: "G-RBHNLGGD1S"
   };
-  // Initialize Firebase
+
   firebase.initializeApp(firebaseConfig);
   firebase.analytics();
 
